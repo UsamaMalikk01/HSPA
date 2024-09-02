48cc8fdd710ee3569ea957e94e7409f7335770ee
dynamit\usama <usama@office.dynamitsolutions.com>
2024-09-02 17:43:56 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
+const appRoutes: Routes = [
+  // {path: '', component: PropertyListComponent},
+  // {path: 'rent-property', component: PropertyListComponent},
+  // {path: 'add-property', component: AddPropertyComponent},
+  // {path: 'property-detail/:id',
+  //     component: PropertyDetailComponent,
+  //     resolve: {prp: PropertyDetailResolverService}},
+  // {path: 'user/login', component: UserLoginComponent},
+  // {path: 'user/register', component: UserRegisterComponent},
+  // {path: '**', component: PropertyListComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+    selector: 'app-property-card',
+    // template: `<h1>I am a card</h1>`,
+    templateUrl: 'property-card.component.html',
+    // styles: ['h1 {font-weight: normal;}'],
+    styleUrls: ['property-card.component.css']
+}
+
+)
+export class PropertyCardComponent {
+    // @Input() property: IPropertyBase;
+    // @Input() hideIcons: boolean;
+
+}
